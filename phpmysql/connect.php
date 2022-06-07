714111353d2df8c26256a12cd4bbfc1556800f6a
Lucas <baiba.lucas@gmail.com>
2022-06-07 15:03:46 +0200

    Db + code connected

--- phpmysql/connect.php
+++ phpmysql/connect.php
@@ -1,8 +1,6 @@
 <?php 
     if($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
-
         $conn= mysqli_connect('localhost', 'root', '', 'test1') or die("Connection Failed:" .mysqli_connect_error());
-
         if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['phone']) && isset($_POST['bgroup'])) {
             $name= $_POST['name'];
             $email= $_POST['email'];
@@ -14,7 +12,8 @@
             $query = mysqli_query($conn,$sql);
             if($query) {
                 echo 'Entry Successfull';
-            } else {
+            }
+            else {
                 echo 'Error Occured';
             }
         }
